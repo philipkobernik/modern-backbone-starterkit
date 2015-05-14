b2a48b5b921aa1117eb9c0e8a47fcae88755a5fe
Philip Kobernik <ptk921@gmail.com>
2015-05-14 16:03:03 -0700

    overwrite Marionette render function to render dust template

--- src/app/main.js
+++ src/app/main.js
@@ -1,9 +1,19 @@
 import $ from 'jquery';
 import Backbone from 'backbone';
+import Marionette from 'backbone.marionette';
 
 import Router from './router';
 
 
 const router = new Router();
+Marionette.Renderer.render = function (template, data) {
+	var html;
+	// Template must be compiled and in the dust cache. Recommend pre-compiling
+	// and loading the templates as scripts at app start.
+	dust.render(template, data, function (err, out) {
+		html = out;
+	});
+	return html;
+};
 
 Backbone.history.start();
